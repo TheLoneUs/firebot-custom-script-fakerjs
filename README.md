3a2faab1f7c1bd17dae2aad18ab6e4b5eec16ff3
MattachineGG <lunsford.matthew@gmail.com>
2025-02-23 15:11:54 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# WARNING: UNDER DEVELOPMENT, NOT TO BE USED YET!
+
+# Firebot FakerJS Custom Script by [MattachineGG](https://allmylinks.com/mattachinegg)
+
+### How to install
+1. Go to the [releases](TODO) tab and download the latest `firebot-custom-script-fakerjs.js`.
+2. Open Firebot and head to Settings -> Scripts -> Manage Startup Scripts.
+3. Click `Add New Script`.
+4. Click on the `scripts folder` link in the popup and place the `firebot-custom-script-fakerjs.js` file inside.
+5. Click the blue reload button next to the `Pick one` dropdown to refresh the available scripts.
+6. Select `firebot-custom-script-fakerjs.js` in the dropdown.
+7. Click `Save` - You might have to restart Firebot for the script to load.
+
+### How to use
+TODO
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { Firebot, ScriptModules } from '@crowbartools/firebot-custom-scripts-types';
+import { autoload } from './autoload';
+import { EventSource } from '@crowbartools/firebot-custom-scripts-types/types/modules/event-manager';
+
+interface Params {}
+
+const script: Firebot.CustomScript<Params> = {
+	getScriptManifest: () => {
+		return {
+			name: 'FakerJS by MattachineGG on Twitch',
+			description: 'Custom script for generating fake data.',
+			author: 'MattachineGG',
+			version: '0.1.0',
+			firebotVersion: '5'
+		};
+	},
+	getDefaultParameters: () => {
+		return {};
+	},
+	run: async runRequest => {
+		const eventSource: EventSource = {
+			id: 'fakerjs',
+			name: 'Fake Data',
+			events: []
+		};
+		autoload(runRequest.modules, eventSource);
+		modules = runRequest.modules;
+
+		modules.frontendCommunicator.on('words-get-methods', () => {
+			return [
+				{
+					method: "word",
+					descriptions: "Get some dan ol words."
+				}
+			];
+		});
+	}
+};
+
+export let modules: ScriptModules;
+export default script;
