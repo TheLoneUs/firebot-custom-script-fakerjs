c7a872b383ebe9832b775baccb63f37afd76d755
MattachineGG <lunsford.matthew@gmail.com>
2025-02-23 19:29:25 -0800

    Working module selector.

--- src/@types/global.d.ts
+++
@@ -1,6 +0,0 @@
-declare const SCRIPTS_DIR: string;
-declare const renderWindow: any;
-declare module '*.html' {
-    const value: string;
-    export default value;
-}
--- src/faker.effect.ts
+++ src/faker.effect.ts
@@ -1,5 +1,4 @@
 import { Effects } from '@crowbartools/firebot-custom-scripts-types/types/effects';
-import template from './faker.html';
 import EffectType = Effects.EffectType;
 
 interface EffectModel {
@@ -23,14 +22,25 @@ const effect: EffectType<EffectModel> = {
 			defaultName: 'fakerData'
 		}]
 	},
-	optionsTemplate: template,
+	optionsTemplate: `
+	<eos-container header="Faker Module" pad-top="true">
+		<div class="btn-group">
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="faker-effect-type">{{effect.module.name ? effect.module.name : 'Pick one'}}</span> <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu faker-effect-dropdown">
+                <li ng-repeat="module in faker_modules"
+                    ng-click="effect.module.name = module.name">
+                    <a href>{{module.name}}</a>
+                </li>
+            </ul>
+        </div>
+		<a href="https://fakerjs.dev/api/" target="_blank"><i class="fad fa-book-open"></i></a>
+	</eos-container>
+	`,
 	optionsController: ($scope, backendCommunicator: any, $q: any) => {
 		const faker_modules = backendCommunicator.fireEventSync('get-faker-modules');
 		$scope.faker_modules = faker_modules;
-
-		if ($scope.effect.module == null) {
-			$scope.effect.module = faker_modules[0];
-		}
 	},
 	onTriggerEvent: async scope => {
 		const effect = scope.effect;
