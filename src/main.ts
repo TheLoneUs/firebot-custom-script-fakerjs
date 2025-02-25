662920ecfd83d8c115fafd5c4e8b3be52da5de09
MattachineGG <lunsford.matthew@gmail.com>
2025-02-25 11:59:33 -0800

    Manually added more of the modules.

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const script: Firebot.CustomScript<Params> = {
 			name: 'MattachineGG\'s Faker',
 			description: 'A custom script for generating fake data.',
 			author: 'MattachineGG',
-			version: '0.9.1',
+			version: '0.9.2',
 			firebotVersion: '5'
 		};
 	},
