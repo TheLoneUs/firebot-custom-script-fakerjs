fad9f505d979aff6164951a141a608e395806267
MattachineGG <lunsford.matthew@gmail.com>
2025-02-28 14:03:35 -0800

    Version bump.

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const script: Firebot.CustomScript<Params> = {
 			name: 'MattachineGG\'s Faker',
 			description: 'A custom script for generating fake data.',
 			author: 'MattachineGG',
-			version: '0.9.5',
+			version: '0.9.6',
 			firebotVersion: '5'
 		};
 	},
